58adc50d75f87324a8aa5ee4b5df9239c766e6fd
hightecular95908 <rkulkin47@gmail.com>
2024-07-29 22:28:25 +0900

    tabBar bottom border added

--- components/tabBar.tsx
+++ components/tabBar.tsx
@@ -15,16 +15,18 @@ const tabs = [
 export default function TabBar() {
     const [tab, setTab] = useState<string>('transfer');
     return (
-        <div className="fixed grid grid-cols-2 w-full h-[60px]">
-            {
-                tabs.map((item, index) => (
-                    <div onClick={() => setTab(item.value)} key={index}>
-                        <button className={`px-4 py-2 bg-transparent rounded-sm hover:opacity-80 transition-all duration-75 ${item.value == tab ? 'border-b-3 border-theme' : ''}`}>
-                            {item.label}
-                        </button>
-                    </div>
-                ))
-            }
+        <div className="fixed w-full bg-[#0c081c] z-10">
+            <div className="grid grid-cols-2 w-full md:w-[1000px] mx-auto border-b-2 border-[#C3C3C3]">
+                {
+                    tabs.map((item, index) => (
+                        <div onClick={() => setTab(item.value)} key={index}>
+                            <button className={`px-4 py-2 rounded-sm hover:opacity-80 transition-all duration-75 ${item.value == tab ? 'border-b-3 border-theme' : ''}`}>
+                                {item.label}
+                            </button>
+                        </div>
+                    ))
+                }
+            </div>
         </div>
     )
 }
